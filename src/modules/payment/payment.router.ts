1ee786c58e57e160fe5cf188af729ac34fad661f
TarekNexus <muhammadtarek5920@gmail.com>
2026-04-05 16:52:27 +0600

    payment router done

--- src/modules/payment/payment.router.ts
+++ src/modules/payment/payment.router.ts
@@ -5,16 +5,32 @@ import { PaymentController } from "./payment.controller";
 const PaymentRouter = express.Router();
 
 // Unified Payment (COD or Stripe)
-PaymentRouter.post("/create", auth(UserRole.CUSTOMER), PaymentController.createPayment);
+PaymentRouter.post(
+  "/create",
+  auth(UserRole.CUSTOMER),
+  PaymentController.createPayment,
+);
 
 // Get My Payments
-PaymentRouter.get("/", auth(UserRole.CUSTOMER), PaymentController.getMyPayments);
+PaymentRouter.get(
+  "/",
+  auth(UserRole.CUSTOMER),
+  PaymentController.getMyPayments,
+);
 
 // Get Payment by ID
-PaymentRouter.get("/:id", auth(UserRole.CUSTOMER), PaymentController.getPaymentById);
+PaymentRouter.get(
+  "/:id",
+  auth(UserRole.CUSTOMER),
+  PaymentController.getPaymentById,
+);
 
 // Webhook
 const PaymentWebhookRouter = express.Router();
-PaymentWebhookRouter.post("/stripe", express.raw({ type: "application/json" }), PaymentController.stripeWebhook);
+PaymentWebhookRouter.post(
+  "/stripe",
+  express.raw({ type: "application/json" }),
+  PaymentController.stripeWebhook,
+);
 
 export { PaymentRouter, PaymentWebhookRouter };
