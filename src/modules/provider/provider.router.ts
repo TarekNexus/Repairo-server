8636cfde8dfe879982a68df087f2086e83cb973d
TarekNexus <muhammadtarek5920@gmail.com>
2026-04-05 16:55:14 +0600

    provider route  done

--- src/modules/provider/provider.router.ts
+++ src/modules/provider/provider.router.ts
@@ -5,10 +5,30 @@ import auth, { UserRole } from "../../middleware/auth";
 const router = Router();
 
 // ===== SERVICES =====
-router.get("/bookings", auth(UserRole.PROVIDER, UserRole.ADMIN), ProviderController.getBookings);
-router.post("/services", auth(UserRole.PROVIDER, UserRole.ADMIN), ProviderController.addService);
-router.put("/services/:id", auth(UserRole.PROVIDER, UserRole.ADMIN), ProviderController.updateService);
-router.delete("/services/:id", auth(UserRole.PROVIDER, UserRole.ADMIN), ProviderController.deleteService);
-router.get("/my-services", auth(UserRole.PROVIDER), ProviderController.getMyServices);
+router.get(
+  "/bookings",
+  auth(UserRole.PROVIDER, UserRole.ADMIN),
+  ProviderController.getBookings,
+);
+router.post(
+  "/services",
+  auth(UserRole.PROVIDER, UserRole.ADMIN),
+  ProviderController.addService,
+);
+router.put(
+  "/services/:id",
+  auth(UserRole.PROVIDER, UserRole.ADMIN),
+  ProviderController.updateService,
+);
+router.delete(
+  "/services/:id",
+  auth(UserRole.PROVIDER, UserRole.ADMIN),
+  ProviderController.deleteService,
+);
+router.get(
+  "/my-services",
+  auth(UserRole.PROVIDER),
+  ProviderController.getMyServices,
+);
 
 export const ProviderRouter: Router = router;
